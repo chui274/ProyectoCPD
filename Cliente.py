4c237e19e219cd533fe3d9e50cf4ca7a844adaed
Sofía Fernandez Moreno <chui274@gmail.com>
2016-01-16 21:38:41 +0100

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -2,23 +2,23 @@ from multiprocessing.connection import Client
 from array import array
 import sqlite3
 #Conexion
-conndb= sqlite3.connect('proyectoCPD2.db')
+conexiondb= sqlite3.connect('proyectoCPD2.db')
 #creamos un cursor, por el cual podremos ejecutar comandos SQL vía el método execute.
-c= conndb.cursor()
+c= conexiondb.cursor()
 
 address = ('compute-0-0',6000)
-conn = Client(address, authkey= 'sofia')
-conn.send('close')
+conexion = Client(address, authkey= 'sofia')
+conexion.send('close')
 leido = conn.recv()
 
 replicado= 1
 c.execute("insert into yahoo values (:symbol, :last, :date, :change, :high, :low, :vol, 1)", leido)
 c.execute("select * from yahoo")
 print c.fetchall()
-conndb.commit()#Guardar cambios
-conndb.close()
+conexiondb.commit()#Guardar cambios
+conexiondb.close()
 
 
 
-conn.close()
+conexion.close()
 print "Conexion exitosa"
