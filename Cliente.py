9977c838cdcbbce6fe04748aac4fad298dd8554a
Sofía Fernandez Moreno <chui274@gmail.com>
2016-01-19 13:32:08 +0100

    Cambios en Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,24 +1,30 @@
 from multiprocessing.connection import Client
 from array import array
 import sqlite3
-#Conexion
-conexiondb= sqlite3.connect('proyectoCPD2.db')
-#creamos un cursor, por el cual podremos ejecutar comandos SQL vía el método execute.
-c= conexiondb.cursor()
+import cPickle 
 
-address = ('compute-0-1',5901)
-conexion = Client(address, authkey= 'sofia')
-conexion.send('close')
-leido = conn.recv()
+	# Conexion
+conndb= sqlite3.connect('cliente.db')
+	# creamos un cursor por el 
+	# cual podremos ejecutar comandos 
+	# SQL via el metodo execute
+c= conndb.cursor()
+# c.execute('''CREATE TABLE yahoo(symbol text, last text, fecha text, change text, high text, low text, vol text, send int)''')
+address = ('localhost',6000)
+conn = Client(address)
+conn.send('close')
+msg = conn.recv()
 
 replicado= 1
-c.execute("insert into yahoo values (:symbol, :last, :date, :change, :high, :low, :vol, 1)", leido)
-c.execute("select * from yahoo")
-print c.fetchall()
-conexiondb.commit()#Guardar cambios
-conexiondb.close()
+
+c.execute('INSERT INTO yahoo values(:symbol, :last, :fecha, :change, :high, :low, :vol)', msg)
+c.execute('SELECT * FROM yahoo')
+resultado = c.fetchall()
+print (resultado)
+conndb.commit()# Guardar cambios
+conndb.close()
 
 
 
-conexion.close()
-print "Conexion exitosa"
+conn.close()
+print ("Conexion exitosa")
